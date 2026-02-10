8110c1289f23bcd5aaf54e128f08501208ad98fa
SHUBHAM SHARMA <83109193+shubhamgaurav90@users.noreply.github.com>
2026-02-10 13:01:10 +0530

    Add files via upload

--- squares.py
+++ squares.py
@@ -1,8 +1,5 @@
 # list comprehension: squares of numbers (1-20) divisible by 2 and 5
 # for a number divisible by 2 and 5 both it should end with 0 that is also divisible by 10 also
 
-for i in range(1,21):
-    if i%10==0:
-        square=i**2
-        print(list([square]))
-        
+squares = [n**2 for n in range(1, 22) if (n**2) % 10 == 0]
+print(squares)
